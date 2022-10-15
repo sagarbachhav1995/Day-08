0aa575f3894c12c41a48cb81b786c5094a00e210
sagarbachhav1995 <sagarbachhav8@gmail.com>
2022-10-15 16:55:49 +0530

    Added: Emp Wage Prog

---
+++ Employeewage.java
@@ -0,0 +1,10 @@
+package com.day08.java;
+
+public class Employeewage {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Welcome to Employee Wage Computation Programe");
+	}
+
+}
