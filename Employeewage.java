0483e3e12e5641a5bd5d24f873cddb1906542a7d
sagarbachhav1995 <sagarbachhav8@gmail.com>
2022-10-15 17:00:54 +0530

    Added: Emp Wage Prog Present or Absent

--- Employeewage.java
+++ Employeewage.java
@@ -1,10 +1,21 @@
 package com.day08.java;
 
+import java.util.Random;
+
 public class Employeewage {
+	public static void empWage() {
+		Random ran = new Random();
+		int PREASENT = 1;
+		int empCheck = ran.nextInt(2);
+		if (empCheck == PREASENT) {
+			System.out.println("Employee is Present");
+		} else {
+			System.out.println("Employee is Absent");
+		}
+	}
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("Welcome to Employee Wage Computation Programe");
-	}
+		System.out.println("Welcome To Employee Wage Computation Using OOP's");
+		empWage();	}
 
 }
