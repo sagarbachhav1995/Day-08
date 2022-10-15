c3d1bce9e8a90162347ca6f9d628a438ce55645d
sagarbachhav1995 <sagarbachhav8@gmail.com>
2022-10-15 17:29:56 +0530

    Added: Monthly Wage

--- Employeewage.java
+++ Employeewage.java
@@ -1,9 +1,10 @@
 package com.day08.java;
-
+import java.util.Random;
 class Employee {
 	static final int IS_FULL_TIME = 1;// constants
 	static final int IS_PART_TIME = 2;// constants
 	static final int EMP_WAGE_PER_DAY = 20;
+	static final int EMP_WAGE_FOR_MONTH = 20;
 
 	/*
 	 * generating random numbers with in 3 using random functions
@@ -17,7 +18,7 @@ class Employee {
 	/*
 	 * calculating employee work hours
 	 */
-	public static void empSalary(int empCheck) {
+	public static int empSalary(int empCheck) {
 		int empWrkHrs;
 		switch (empCheck) {
 		case IS_FULL_TIME -> empWrkHrs = 8;
@@ -26,6 +27,24 @@ class Employee {
 		}
 		int empWage = empWrkHrs * EMP_WAGE_PER_DAY;
 		System.out.println("empWage = " + empWage);
+		return empWage;
+	}
+
+	/*
+	 * calculating employee total salary
+	 */
+	public static int totalEmpSalary() {
+		int day = 0;
+		int totalEmpSalary = 0;
+		int empWage = 0;
+		int empCheck = 0;
+		while (day < EMP_WAGE_FOR_MONTH) {
+			empCheck = empCheck();
+			empWage = empSalary(empCheck);
+			++day;
+			totalEmpSalary += empWage;
+		}
+		return totalEmpSalary;
 	}
 }
 
@@ -33,8 +52,9 @@ class Employee {
 // Main Methods
 public class Employeewage {
 	public static void main(String[] args) {
-		System.out.println("Welcome To Employee Wage Computation Using OOP's Concept");
-		int empCheck = Employee.empCheck();
-		Employee.empSalary(empCheck);	}
+		int totalEmpSalary;
+		System.out.println("Welcome To Employee Wage Computation Using OOP's");
+		totalEmpSalary = Employee.totalEmpSalary();
+		System.out.println("totalEmpSalary = " + totalEmpSalary);	}
 
 }
