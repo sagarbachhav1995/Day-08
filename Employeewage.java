a2616c022c83d875f2cbc4943b94612ac701d002
sagarbachhav1995 <sagarbachhav8@gmail.com>
2022-10-15 17:26:08 +0530

    Added: Using Switch Case

--- Employeewage.java
+++ Employeewage.java
@@ -1,37 +1,40 @@
 package com.day08.java;
 
-import java.util.Random;
-
 class Employee {
-	static final int IS_FULL_TIME = 1;
-	static final int IS_PART_TIME = 2;
+	static final int IS_FULL_TIME = 1;// constants
+	static final int IS_PART_TIME = 2;// constants
 	static final int EMP_WAGE_PER_DAY = 20;
 
+	/*
+	 * generating random numbers with in 3 using random functions
+	 */
 	public static int empCheck() {
 		Random random = new Random();
 		int empCheck = random.nextInt(3);
 		return empCheck;
 	}
 
+	/*
+	 * calculating employee work hours
+	 */
 	public static void empSalary(int empCheck) {
-		int empWrkHrs = 0;
-		if (empCheck == IS_FULL_TIME) {
-			empWrkHrs = 8;
-		} else if (empCheck == IS_PART_TIME) {
-			empWrkHrs = 4;
-		} else {
-			empWrkHrs = 0;
+		int empWrkHrs;
+		switch (empCheck) {
+		case IS_FULL_TIME -> empWrkHrs = 8;
+		case IS_PART_TIME -> empWrkHrs = 4;
+		default -> empWrkHrs = 0;
 		}
 		int empWage = empWrkHrs * EMP_WAGE_PER_DAY;
 		System.out.println("empWage = " + empWage);
 	}
 }
 
+// Main Class
+// Main Methods
 public class Employeewage {
 	public static void main(String[] args) {
-		System.out.println("Welcome To Employee Wage Computation Using OOP's");
+		System.out.println("Welcome To Employee Wage Computation Using OOP's Concept");
 		int empCheck = Employee.empCheck();
-		Employee.empSalary(empCheck);
-	}
+		Employee.empSalary(empCheck);	}
 
 }
